7a425fa805e727e66abbf66653751b92bbed4695
Andie Vester <avester146@gmail.com>
2022-08-13 20:52:34 -0400

    Configuring for heroku

--- server/index.js
+++ server/index.js
@@ -13,9 +13,16 @@ app.use(bodyParser.json({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/poem', poemRoutes);
+app.use('/api/poem', poemRoutes);
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT || 3000; 
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+    res.sendFile(path.join('poetry-app' + '/client/build/index.html'));
+    });
+   }
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
