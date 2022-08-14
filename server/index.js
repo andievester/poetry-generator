2620af0df70cafc07f514a2aa91011cfd1cc33ed
Andie Vester <avester146@gmail.com>
2022-08-13 22:01:57 -0400

    Removing change that broke routes

--- server/index.js
+++ server/index.js
@@ -13,16 +13,9 @@ app.use(bodyParser.json({ extended: true }))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/api/poem', poemRoutes);
+app.use('/poem', poemRoutes);
 
-const PORT = process.env.PORT || 3000; 
-
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-    res.sendFile(path.join('poetry-app' + '/client/build/index.html'));
-    });
-   }
+const PORT = process.env.PORT; 
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
