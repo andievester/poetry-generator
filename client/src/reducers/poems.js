b83a84da265bba1d73828903fda6b81c17d35288
Andie Vester <avester146@gmail.com>
2022-12-29 15:58:52 -0500

    added progress spinner

--- client/src/reducers/poems.js
+++ client/src/reducers/poems.js
@@ -1,33 +1,39 @@
-const defaultPoem = new Object({
-    title: String,
-    author: Object({
-        first: String,
-        last: String
-    }),
-    lines: [String],
-    stanzas: [[String]],
-});
+const defaultPoem = {
+  title: String,
+  author: Object({
+    first: String,
+    last: String,
+  }),
+  lines: [String],
+  stanzas: [[String]],
+  isLoading: Boolean,
+};
 
-defaultPoem.title = '';
-defaultPoem.author.first = '';
-defaultPoem.author.last = '';
-defaultPoem.lines = [''];
-defaultPoem.stanzas = [['']]; 
+defaultPoem.title = "";
+defaultPoem.author.first = "";
+defaultPoem.author.last = "";
+defaultPoem.lines = [""];
+defaultPoem.stanzas = [[""]];
+defaultPoem.isLoading = false;
 
-const reducer = (state = defaultPoem, action ) => {
-    if (action) {
-        switch (action.type) {
-            case 'FETCH_ALL':
-                return action.payload; 
-            case 'FETCH':
-                return action.payload;  
-            default:  
-            return state;
-        }
-    } else {
+const reducer = (state = defaultPoem, action) => {
+  if (action) {
+    switch (action.type) {
+      case "FETCH_ALL":
+        return action.payload;
+      case "FETCH":
+        return action.payload;
+      case "LOADER":
+        return {
+          ...state,
+          isLoading: action.payload,
+        };
+      default:
         return state;
     }
-    
-}
- 
+  } else {
+    return state;
+  }
+};
+
 export default reducer;
